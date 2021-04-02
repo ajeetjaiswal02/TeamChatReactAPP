0495057cff77aa03792cb9994bb8be000217da14
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-04-02 12:31:32 +0530

    add basic working project phase 1

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqC7Ib2xiSK08B1hkM02LriQHbhOvOvdY",
+    authDomain: "team-chatter.firebaseapp.com",
+    projectId: "team-chatter",
+    storageBucket: "team-chatter.appspot.com",
+    messagingSenderId: "974844024207",
+    appId: "1:974844024207:web:1617ad461fe0b11319a7fa"
+};
